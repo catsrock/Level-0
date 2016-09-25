403edf06ff884c9e14563287aeb6ab5d0dd9bfeb
Leauge Mac <DONT CHANGE THIS!!!!!>
2016-09-25 16:01:55 -0700

    Whoo-hoo!
    Class was great today!

---
+++ src/sleepyhead.java
@@ -0,0 +1,24 @@
+// Copyright Wintriss Technical Schools 2013
+import javax.swing.JOptionPane;
+
+public class sleepyhead {
+
+	public static void main(String[] args) {
+		
+		boolean isWeekday, isVacation;
+		int eight=JOptionPane.showConfirmDialog(null, "Is it a weekday?","",JOptionPane.YES_NO_OPTION);
+		
+		/*
+		 * Ask the user for these values using
+		 * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
+		 * JOptionPane.YES_NO_OPTION);
+		 */
+	
+
+		/*
+		 * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
+		 * print “get up lazybones!” If it is a weekday, and we are on vacation,
+		 * print “sleep in”.
+		 */
+	}
+}
---
+++ src/zodiacsign.java
@@ -0,0 +1,49 @@
+import javax.swing.JOptionPane;
+
+public class zodiacsign {public static void main(String[] args) {
+	String zodiac=JOptionPane.showInputDialog("What is your zodiac sign?");
+	if(zodiac.equals("aries")){
+		JOptionPane.showMessageDialog(null, "You will lose a million dollars that you never knew you had.");
+	}
+	else if(zodiac.equals("taurus")){
+		JOptionPane.showMessageDialog(null, "You will gain a million dollars from someone with an Aries symbol.");
+	
+	}
+	else if(zodiac.equals("gemini")){
+		JOptionPane.showMessageDialog(null, "You make a new discovery and become famous");
+	}
+	else if(zodiac.equals("cancer")){
+	JOptionPane.showMessageDialog(null, "You will find that your loyalty to your friends will pay off later on.");
+	}
+	else if(zodiac.equals("leo")){
+		JOptionPane.showMessageDialog(null, "You will get a scholarship and graduate from Stanford.");
+	}
+	else if(zodiac.equals("virgo")){
+		JOptionPane.showMessageDialog(null, "Your shyness and stress will overcome you.");
+	}
+	else if(zodiac.equals("libra")){
+		JOptionPane.showMessageDialog(null, "Your peace will find success.");
+	}
+	else if(zodiac.equals("scorpio")){
+		JOptionPane.showMessageDialog(null, "You will become a coding teacher.");
+	}
+	else if(zodiac.equals("sagittarius")){
+		JOptionPane.showMessageDialog(null, "You will travel all over the world and settle down in South Korea.");
+	}
+	else if(zodiac.equals("capricorn")){
+		JOptionPane.showMessageDialog(null, "You will become a successful, but strict manager that gets rid of lazy workers.");
+	}
+	else if(zodiac.equals("aqrarius")){
+		JOptionPane.showMessageDialog(null, "You will become a promising public speaker.");
+	}
+	else if(zodiac.equals("pisces")){
+		JOptionPane.showMessageDialog(null, "You escape reality by immersing yourself in art.");
+	}
+	
+}
+
+
+
+
+
+}
